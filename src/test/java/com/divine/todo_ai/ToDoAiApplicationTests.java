6f11ec2d35c466acfb00d47c73cbeba469a47ae9
Divine Aghulor <divineaghulor04@gmail.com>
2025-06-22 19:22:02 +0100

    first commit
    This has all the AI generated files
    It's not working yet because of an issue in JwtUtil anyway. Probably a version issue

---
+++ src/test/java/com/divine/todo_ai/ToDoAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.divine.todo_ai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToDoAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
